d7d01bebf6138260d7d17d982ec3b3a4e3a436f8
arjolaca11 <arjolaca@gmail.com>
2026-05-20 20:43:35 -0600

    SEM01

---
+++ pruebaTechShop/src/main/java/pruebaTechShop/Armando/PruebaTechShopArmandoApplication.java
@@ -0,0 +1,13 @@
+package pruebaTechShop.Armando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTechShopArmandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTechShopArmandoApplication.class, args);
+	}
+
+}
